faccf84167d09febffe5a0f53f09a6608e33bb62
khaylinha <168603388+khaylinha@users.noreply.github.com>
2024-05-06 20:53:43 -0300

    adicionando arquivos js e css

---
+++ styl.css/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    
+   stroke( blue )
+    fill( red )
+    
+    if( mauseISPressed)}
+      rect(mauseX, mauseY, 20, 35)
+   }
+  
+}
